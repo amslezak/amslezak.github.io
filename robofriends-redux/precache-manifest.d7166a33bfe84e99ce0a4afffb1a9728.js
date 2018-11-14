b4485dc5ab1252578bde3843196628003ffde33f
Andy Slezak <andy.slezak@gmail.com>
2018-11-13 23:02:02 -0500

    adds robofriends-redux

---
+++ robofriends-redux/precache-manifest.d7166a33bfe84e99ce0a4afffb1a9728.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed380602f12d25e8e72e",
+    "url": "/static/js/main.ed380602.chunk.js"
+  },
+  {
+    "revision": "26ac9b31d6cbb1767cb1",
+    "url": "/static/js/1.26ac9b31.chunk.js"
+  },
+  {
+    "revision": "ed380602f12d25e8e72e",
+    "url": "/static/css/main.b481d27b.chunk.css"
+  },
+  {
+    "revision": "26ac9b31d6cbb1767cb1",
+    "url": "/static/css/1.dcb71130.chunk.css"
+  },
+  {
+    "revision": "48283f036afa66505583e21d738149e6",
+    "url": "/index.html"
+  }
+];
